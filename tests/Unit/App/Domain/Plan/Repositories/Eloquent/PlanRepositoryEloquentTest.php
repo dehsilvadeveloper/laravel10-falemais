f6ec4b37c41839af24c55e3fee43425c89ce255b
dehsilva <andre.silva.clinica@gmail.com>
2024-03-19 10:17:02 -0300

    Finishing tests scenarios for test class PlanRepositoryEloquentTest

--- tests/Unit/App/Domain/Plan/Repositories/Eloquent/PlanRepositoryEloquentTest.php
+++ tests/Unit/App/Domain/Plan/Repositories/Eloquent/PlanRepositoryEloquentTest.php
@@ -141,4 +141,97 @@ class PlanRepositoryEloquentTest extends TestCase
         $this->assertCount(0, $records);
         $this->assertTrue($records->isEmpty());
     }
+
+    /**
+     * @group repositories
+     * @group plan
+     */
+    public function test_can_get_list_filtered_by_field(): void
+    {
+        $recordsCount = 3;
+
+        $generatedRecords = Plan::factory()->count($recordsCount)->create([
+            'max_free_minutes' => 15
+        ]);
+        $generatedRecordsAsArray = $generatedRecords->toArray();
+
+        $records = $this->repository->getByField('max_free_minutes', 15);
+        $recordsAsArray = $records->toArray();
+
+        $this->assertCount($recordsCount, $records);
+
+        for ($i = 0; $i <= ($recordsCount - 1); $i++) {
+            $this->assertEquals($generatedRecordsAsArray[$i]['name'], $recordsAsArray[$i]['name']);
+            $this->assertEquals(
+                $generatedRecordsAsArray[$i]['max_free_minutes'],
+                $recordsAsArray[$i]['max_free_minutes']
+            );
+        }
+    }
+
+    /**
+     * @group repositories
+     * @group plan
+     */
+    public function test_can_get_empty_list_filtered_by_field(): void
+    {
+        $records = $this->repository->getByField('max_free_minutes', 15);
+
+        $this->assertCount(0, $records);
+        $this->assertTrue($records->isEmpty());
+    }
+
+    /**
+     * @group repositories
+     * @group plan
+     */
+    public function test_can_find_by_id(): void
+    {
+        $existingRecord = Plan::factory()->create();
+
+        $foundRecord = $this->repository->firstById($existingRecord->id);
+
+        $this->assertInstanceOf(Plan::class, $foundRecord);
+        $this->assertEquals($existingRecord->name, $foundRecord->name);
+        $this->assertEquals($existingRecord->max_free_minutes, $foundRecord->max_free_minutes);
+    }
+
+    /**
+     * @group repositories
+     * @group plan
+     */
+    public function test_cannot_find_by_id_a_nonexistent_record(): void
+    {
+        $foundRecord = $this->repository->firstById(1);
+
+        $this->assertNull($foundRecord);
+    }
+
+    /**
+     * @group repositories
+     * @group plan
+     */
+    public function test_can_find_by_field(): void
+    {
+        $existingRecord = Plan::factory()->create([
+            'max_free_minutes' => 5
+        ]);
+
+        $foundRecord = $this->repository->firstByField('max_free_minutes', 5);
+
+        $this->assertInstanceOf(Plan::class, $foundRecord);
+        $this->assertEquals($existingRecord->name, $foundRecord->name);
+        $this->assertEquals($existingRecord->max_free_minutes, $foundRecord->max_free_minutes);
+    }
+
+    /**
+     * @group repositories
+     * @group plan
+     */
+    public function test_cannot_find_by_field_a_nonexistent_record(): void
+    {
+        $foundRecord = $this->repository->firstByField('max_free_minutes', 5);
+
+        $this->assertNull($foundRecord);
+    }
 }
