ac437c8e820c4cf7e5d953a3e430b42592e1d705
dehsilva <andre.silva.clinica@gmail.com>
2024-03-21 16:56:36 -0300

    Creating service CalculateCallPriceService.

---
+++ app/Domain/CallPrice/Services/CalculateCallPriceService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Domain\CallPrice\Services;
+
+use Illuminate\Http\Response;
+use App\Domain\CallPrice\DataTransferObjects\CallPriceCalculationDto;
+
+class CalculateCallPriceService
+{
+    public function calculateWithPlan(CallPriceCalculationDto $dto): float
+    {
+        $dataArray = $dto->toArray();
+        list($callMinutes, $exceedingFeePercentage, $farePricePerMinute, $planMaxFreeMinutes) = $dataArray;
+
+        if (!$planMaxFreeMinutes) {
+            throw new \Exception(
+                'Cannot proceed the calculation without a valid value for plan max free minutes.',
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        return (($callMinutes - $planMaxFreeMinutes) * $farePricePerMinute) 
+            + (($exceedingFeePercentage / 100) * $farePricePerMinute);
+    }
+
+    public function calculateWithoutPlan(CallPriceCalculationDto $dto): float
+    {
+        $dataArray = $dto->toArray();
+        list($callMinutes, $farePricePerMinute) = $dataArray;
+
+        return $callMinutes * $farePricePerMinute;
+    }
+}
